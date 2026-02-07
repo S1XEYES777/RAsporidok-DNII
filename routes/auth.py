0019e5b54ce4a7dedcee63332d06c48e9ebc3cea
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 12:23:32 +0500

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,1 +1,25 @@
+from flask import Blueprint, render_template, request, redirect
+from models import db, User
+
+auth_bp = Blueprint("auth", __name__)
+
+@auth_bp.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        name = request.form["name"]
+
+        user = User.query.filter_by(name=name).first()
+
+        if not user:
+            user = User(name=name)
+
+            if name == "Admin0987654321":
+                user.is_admin = True
+
+            db.session.add(user)
+            db.session.commit()
+
+        return redirect("/main")
+
+    return render_template("login.html")
 
