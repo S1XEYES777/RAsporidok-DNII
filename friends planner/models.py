fbd970b8b0f06525b975b811ae000558f4385d16
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 13:07:57 +0500

    Update models.py

--- friends planner/models.py
+++ friends planner/models.py
@@ -1,15 +1,13 @@
-
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from datetime import datetime
 
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     avatar = db.Column(db.String(100), default="default.png")
     is_admin = db.Column(db.Boolean, default=False)
-
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,19 +15,20 @@ class Task(db.Model):
     date = db.Column(db.String(20))
     difficulty = db.Column(db.Integer)
     completed = db.Column(db.Boolean, default=False)
-
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200))
-
-    user_id = db.Column(db.Integer)
     task_id = db.Column(db.Integer)
-
+    user_id = db.Column(db.Integer)
 
 class Congrat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
     task_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    user_id = db.Column(db.Integer)
