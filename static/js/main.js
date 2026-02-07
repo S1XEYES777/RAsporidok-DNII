ce17df01eae02e91cfeee2c7ceeb68ac6edb06c6
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 14:55:06 +0500

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -10,7 +10,7 @@ data.forEach(u=>{
 html+=`
 <div class="col-md-4 col-12 mb-3">
 
-<div class="card glass p-3">
+<div class="card p-3">
 
 <div class="d-flex align-items-center mb-2">
 <img src="/static/avatars/${u.avatar}" class="avatar">
@@ -24,9 +24,10 @@ u.tasks.forEach(t=>{
 
 html+=`
 <div class="task diff${t.difficulty}">
-${t.text}
 
-<button onclick="complete(${t.id})" class="btn btn-sm btn-light">✔</button>
+<span>${t.text}</span>
+
+<span>${t.completed ? "✅" : ""}</span>
 
 </div>
 `
@@ -41,13 +42,6 @@ html+=`</div></div>`
 })
 
 document.getElementById("cards").innerHTML=html
-}
-
-async function complete(id){
-
-await fetch("/complete/"+id)
-document.getElementById("doneSound").play()
-load()
 }
 
 setInterval(load,3000)
