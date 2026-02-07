69e92bb478e0a09c731da2d709b991cfebfed082
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 16:17:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from routes.auth import auth_bp
 from routes.main import main_bp
 from routes.profile import profile_bp
 from routes.admin import admin_bp
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,8 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    # гарантируем папку для аватаров
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(main_bp)
