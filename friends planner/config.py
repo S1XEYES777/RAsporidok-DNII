3844efc93a26487b3228fef287250634e2b230e8
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 13:23:05 +0500

    Delete friends planner/config.py

--- friends planner/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "static/avatars"
