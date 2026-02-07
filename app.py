35d781e83c8956a3f6a92789854efcdadfc833aa
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 19:40:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,21 @@ from routes.auth import auth_bp
 from routes.main import main_bp
 from routes.profile import profile_bp
 from routes.admin import admin_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Фикс для Render / прокси
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
+
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    # гарантируем папку для аватаров
+
+    # Создаём папку для аватаров
     os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 app.register_blueprint(auth_bp)
