c90342f87e80631f3ef92cb6fa783c08f98f187e
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 13:23:28 +0500

    Update and rename app2.py to models.py

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    avatar = db.Column(db.String(100), default="default.png")
+    is_admin = db.Column(db.Boolean, default=False)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    date = db.Column(db.String(20))
+    difficulty = db.Column(db.Integer)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    task_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+class Congrat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    user_id = db.Column(db.Integer)
