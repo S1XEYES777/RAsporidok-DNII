e48b40b080dd073f73cd10c4f1d10040b5d58da4
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 16:17:02 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///database.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/avatars"
+    MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB
