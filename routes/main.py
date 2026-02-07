3dd497ddc473a3c958877000aa0db300a57adbbb
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 13:50:29 +0500

    Update main.py

--- routes/main.py
+++ routes/main.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template, session, redirect
-from models import User, Task
-
+from flask import Blueprint, render_template, session, redirect, jsonify, request
+from models import User, Task, Congrat, Comment
 from datetime import date
 
 main_bp = Blueprint("main", __name__)
@@ -11,18 +10,43 @@ def main():
     if "user_id" not in session:
         return redirect("/")
 
+    return render_template("main.html")
+
+
+@main_bp.route("/api/data")
+def data():
+
     today = str(date.today())
     users = User.query.all()
 
-    data = []
+    result = []
 
     for u in users:
+
         tasks = Task.query.filter_by(user_id=u.id,date=today).all()
 
-        data.append({
-            "user":u,
-            "tasks":tasks
+        result.append({
+            "name":u.name,
+            "avatar":u.avatar,
+            "tasks":[
+                {
+                    "id":t.id,
+                    "text":t.text,
+                    "difficulty":t.difficulty,
+                    "completed":t.completed
+                }
+                for t in tasks
+            ]
         })
 
-    return render_template("main.html", data=data)
+    return jsonify(result)
 
+
+@main_bp.route("/complete/<int:id>")
+def complete(id):
+
+    task = Task.query.get(id)
+    task.completed = not task.completed
+    db.session.commit()
+
+    return "ok"
