5f8389f57e3c4fcf876b49ed7294496809464745
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 19:45:12 +0500

    Update models.py

--- models.py
+++ models.py
@@ -5,23 +5,22 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-    avatar = db.Column(db.String(120), default="default.png")
+    name = db.Column(db.String(50), unique=True)
+
+    avatar = db.Column(db.Text, nullable=True)  # base64
+
     is_admin = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(220), nullable=False)
-    date = db.Column(db.String(10), nullable=False)  # YYYY-MM-DD
-    difficulty = db.Column(db.Integer, default=3)    # 1..6
+    text = db.Column(db.String(220))
+    date = db.Column(db.String(20))
+    difficulty = db.Column(db.Integer)
     completed = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
 class Congrat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)   # кто поздравил
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    task_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
