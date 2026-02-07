8ef43efe89c23c92c24ec442c1aef74a646dac85
S1XEYES777 <anuartemirgaliev92@gmail.com>
2026-02-07 19:39:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///database.db")
+
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
+
+    db_url = os.environ.get("DATABASE_URL")
+
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url or "sqlite:///database.db"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/avatars"
-    MAX_CONTENT_LENGTH = 2 * 1024 * 1024  # 2MB
